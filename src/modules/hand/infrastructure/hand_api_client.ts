4c0f9a5aca46603fe0bf84a6130832ba8d81c55e
Leopoldo <leopoldo.romero@powen.es>
2025-03-08 13:46:31 +0100

    debuging deploy error

--- src/modules/hand/infrastructure/hand_api_client.ts
+++ src/modules/hand/infrastructure/hand_api_client.ts
@@ -1,4 +1,4 @@
-import { AxiosError } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 import { Hand } from '../domain/hand';
 import { HandRepository } from '../domain/hand.repository';
 import { HandDto, PlayersStatsDto } from './history_parser_api.response';
@@ -6,14 +6,11 @@ import { handDtoToDomainMapper } from './history_parser_api_to_domain.mapper';
 import { cookies } from 'next/headers';
 import { PlayerStats } from '../domain/player_stats';
 import { v4 as uuidv4 } from 'uuid';
-import { getAxiosHttpClient } from '@/modules/shared/infrastructure/axios_http_client';
 interface GetHandResponse {
   hand: HandDto;
   prev_hand_id: string;
   next_hand_id: string;
 }
-
-const axiosHttpClient = getAxiosHttpClient();
 
 export class HandApiClient implements HandRepository {
   private readonly API_URL = process.env.API_URL;
@@ -29,7 +26,7 @@ export class HandApiClient implements HandRepository {
         userId = uuidv4();
         cookieStore.set('user_id', userId);
       }
-      await axiosHttpClient.post<FormData, void>(`${this.API_URL}/api/v1/hands`, formData, {
+      await axios.post<FormData, void>(`${this.API_URL}/api/v1/hands`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
           'Access-Control-Allow-Origin': 'localhost:8000',
@@ -60,7 +57,7 @@ export class HandApiClient implements HandRepository {
       userId = uuidv4();
       cookieStore.set('user_id', userId);
     }
-    const response = await axiosHttpClient.get<GetHandResponse>(
+    const response = await axios.get<AxiosResponse<GetHandResponse>>(
       `${this.API_URL}/api/v1/hands/${id}`,
       {
         headers: {
@@ -70,7 +67,7 @@ export class HandApiClient implements HandRepository {
         withCredentials: true,
       }
     );
-
+    console.log('[[HandsApiClient get hand response]]:', response?.data)
     const playeerStats = await this.getStats();
     // TODO: the next lines are usefull to test full ring styles
     // response.data.hand.players.push(...response.data.hand.players.slice(0,3).map((el) => ({
@@ -79,9 +76,9 @@ export class HandApiClient implements HandRepository {
     //     name: `${el.name}-${Math.random().toFixed(0)}`
     // })))
     return {
-      hand: handDtoToDomainMapper(response?.hand, playeerStats),
-      prevHandId: response?.prev_hand_id,
-      nextHandId: response?.next_hand_id,
+      hand: handDtoToDomainMapper(response?.data?.data?.hand, playeerStats),
+      prevHandId: response?.data?.data?.prev_hand_id,
+      nextHandId: response?.data?.data?.next_hand_id,
     };
   }
 
@@ -93,7 +90,7 @@ export class HandApiClient implements HandRepository {
       userId = uuidv4();
       cookieStore.set('user_id', userId);
     }
-    const response = await axiosHttpClient.get<Array<HandDto>>(
+    const response = await axios.get<AxiosResponse<Array<HandDto>>>(
       `${this.API_URL}/api/v1/hands`,
       {
         headers: {
@@ -103,7 +100,8 @@ export class HandApiClient implements HandRepository {
         withCredentials: true,
       }
     );
-    const hands: Array<Hand> = response?.map((handDto) =>
+    console.log('[[HandsApiClient get hands response]]:', response?.data)
+    const hands: Array<Hand> = response?.data?.data?.map((handDto) =>
       handDtoToDomainMapper(handDto)
     );
     return hands;
@@ -117,7 +115,7 @@ export class HandApiClient implements HandRepository {
       userId = uuidv4();
       cookieStore.set('user_id', userId);
     }
-    const response = await axiosHttpClient.get<PlayersStatsDto>(
+    const response = await axios.get<AxiosResponse<PlayersStatsDto>>(
       `${this.API_URL}/api/v1/stats`,
       {
         headers: {
@@ -127,9 +125,10 @@ export class HandApiClient implements HandRepository {
         withCredentials: true,
       }
     );
+    console.log('[[HandsApiClient get stats response]]:', response?.data)
     const playerStats: PlayerStats = {};
 
-    Object.entries(response).forEach(([player, stats]) => {
+    Object.entries(response.data?.data).forEach(([player, stats]) => {
       playerStats[player] = {
         hands: stats?.hands,
         vpip: stats?.vpip,
