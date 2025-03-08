887db7eb4192add391bb0e940339604a4c570151
Leopoldo <leopoldo.romero@powen.es>
2025-03-08 13:07:47 +0100

    feature/P-001 create lazy initiation for axios http client

--- src/actions/get_players_stats.action.ts
+++
@@ -1,7 +0,0 @@
-'use server';
-import { PlayerStats } from '@/modules/hand/domain/player_stats';
-import { handApiClient } from '@/modules/hand/infrastructure/hand_api_client';
-
-export async function getPlayersStats(): Promise<PlayerStats | null> {
-  return handApiClient.getStats();
-}
--- src/actions/upload_history.action.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-import { Hand } from '@/modules/hand/domain/hand';
-import { HistoryParserApiClient } from '@/modules/hand/infrastructure/history_parser_api_client';
-
-const parserApiClient = new HistoryParserApiClient();
-
-export async function uploadHistoryAction(file: File): Promise<Array<Hand>> {
-  return parserApiClient.parse(file);
-}
