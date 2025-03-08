80ebfd183043b11bd94071dcfb42f26f19480239
Leopoldo <leopoldo.romero@powen.es>
2025-03-08 13:19:38 +0100

    feature/P-001 change http client axios use to debug error

--- src/modules/shared/infrastructure/axios_http_client.ts
+++ src/modules/shared/infrastructure/axios_http_client.ts
@@ -3,22 +3,25 @@ import { ApiConfig, HttpClient, HttpClientResponse } from "@/modules/hand/domain
 import axios, { AxiosInstance, AxiosResponse } from "axios";
 
 export class AxiosHttpClient implements HttpClient {
-    protected client: AxiosInstance;
+    // protected client: AxiosInstance;
 
-    public constructor() {
-        this.client = axios.create();
-        this.client.interceptors.response.use(this.handleResponse);
-        this.client.interceptors.request.use(this.handleRequest);
-    }
+    // public constructor() {
+    //     this.client = axios.create();
+    //     this.client.interceptors.response.use(this.handleResponse);
+    //     this.client.interceptors.request.use(this.handleRequest);
+    // }
 
     async get<R>(url: string, config?: ApiConfig): Promise<R> {
-        const response: AxiosResponse<R> = await this.client.get(url, config);
+        console.log('[[Axios GET request]]:', url, config)
+        const response: AxiosResponse<R> = await axios.get(url, config);
+        console.log('[[Axios GET response]]:', response?.data)
         return response.data;
     }
 
     async post<T = any, R = HttpClientResponse<T>>(url: string, data?: T, config?: ApiConfig): Promise<R> {
-        const response: AxiosResponse<R> = await this.client.post(url, data, config);
-        console.log('[[Axios POST reqwuest]]:', url, data, config)
+        console.log('[[Axios POST request]]:', url, data, config)
+        const response: AxiosResponse<R> = await axios.post(url, data, config);
+        console.log('[[Axios POST response]]:', response?.data)
         return response.data;
     }
 
